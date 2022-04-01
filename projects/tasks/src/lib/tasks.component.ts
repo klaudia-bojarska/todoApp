1c347c5bfaa452547c2a3b91a871a48eff2116f1
klaudiabojarska <klaudia.bojarska@cobiro.com>
2022-04-01 12:06:13 +0200

    initial commit

---
+++ projects/tasks/src/lib/tasks.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-tasks',
+  template: `
+    <p>
+      tasks works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
